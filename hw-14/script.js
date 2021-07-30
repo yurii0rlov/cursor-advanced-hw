70491b9637ce07cfcf6209efa9dd26e03d95dcbd
yurii0rlov <80713098+yurii0rlov@users.noreply.github.com>
2021-07-30 10:18:51 +0300

    Finished HW-14

---
+++ hw-14/script.js
@@ -0,0 +1,57 @@
+//Створіть нескінченний генератор ідентифікаторів.
+
+function* createIdGenerator() {
+  for (let i = 1; ; i++){
+    yield i;
+  }
+}
+
+const idGenerator = createIdGenerator();
+
+let numberString = '';
+
+document.getElementById('addNumber').addEventListener('click', function(){
+  const div = document.getElementById('generateNumbers');
+  div.innerHTML = '';
+  numberString += String(idGenerator.next().value + ', ');
+  div.insertAdjacentHTML('afterbegin', `<p>${numberString}</p>`)
+})
+
+// advanced
+
+function* newFontGenerator(fontSize) {
+  let currentFontSize = fontSize;
+  let fontSizeDirection = '';
+
+  for (let i = 1; ; i++) {
+
+    if (fontSizeDirection === 'up') {
+      currentFontSize = currentFontSize + 2;
+      fontSizeDirection = yield currentFontSize;
+      continue;
+    }
+
+    if (fontSizeDirection === 'down') {
+      currentFontSize = currentFontSize - 2;
+      fontSizeDirection = yield currentFontSize;
+      continue;
+    }
+
+    else {
+      fontSizeDirection = yield currentFontSize;
+      continue;
+    }
+  }
+}
+
+const fontGenerator = newFontGenerator(14);
+
+document.getElementById('smallerFont').addEventListener('click', function(){
+  const div = document.getElementById('generateNumbers');
+  div.style.fontSize = `${fontGenerator.next('down').value}pt`
+});
+
+document.getElementById('biggerFont').addEventListener('click', function(){
+  const div = document.getElementById('generateNumbers');
+  div.style.fontSize = `${fontGenerator.next('up').value}pt`
+});
